c4b5c100e26f5b4c56e18c27b999b4913c95e01c
Mehmet ÖZTÜRK <mozturk@parta.com.tr>
2021-06-04 01:22:12 +0300

    MERN TEST

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan'); //Morgan give info about each request
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+require('dotenv').config({
+    path: './config/index.env'
+});
+
+//MongoDB
+const connectDB = require('./config/db');
+connectDB();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+//routes
+app.use('/api/user/', require('./routes/auth.route'))
+
+app.get('/',(req, res) => {
+    res.send('test route => home page');
+});
+
+//Page not found
+app.use((req, res)=> {
+    res.status(404).json({
+        msg : 'Page not found'
+    })
+});
+
+const PORT = process.env.PORT
+
+app.listen(PORT,() => {
+    console.log(`Server running on Port ${PORT}`)
+}) 
